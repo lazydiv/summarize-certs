20aec02e552885ef82c5d1afc2644dfc575966d8
lazydiv <yhiazedan555@gmail.com>
2024-08-26 06:24:14 +0300

    final

--- components/nav.tsx
+++ components/nav.tsx
@@ -12,7 +12,7 @@ export async function Nav() {
         <header className="z-50 w-full border-b  border-border bg-background backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="container flex h-16 max-w-screen-2xl justify-between   items-center">
                 <Link href="/" className="lg:w-[33%] flex space-x-2 justif">
-                <Image src={'https://utfs.io/f/4f406d7d-151d-4e92-9192-e92de15307af-r0grkz.png'} alt="logo" width={40} className="dark:hidden mt-1" height={40} />
+                <Image src={'https://utfs.io/f/4f406d7d-151d-4e92-9192-e92de15307af-r0grkz.png'} alt="logo" width={40} className="dark:hidden -mt-1" height={40} />
             <Image src={'https://utfs.io/f/3a3c971f-9026-46f3-b38c-5d3af5b27c1e-g0b6r3.png'} alt="logo" width={40} className="hidden dark:inline mt-1" height={40} />
 
 
